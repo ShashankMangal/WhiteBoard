1cfd9a21ae925c32ba5a26c68ae15bc59b26b11e
Shashank Mangal <shashankmangal10@gmail.com>
2021-04-30 20:00:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/whiteboard/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.whiteboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+import me.panavtec.drawableview.DrawableView;
+import me.panavtec.drawableview.DrawableViewConfig;
+
+public class MainActivity extends AppCompatActivity {
+    DrawableView drawableView;
+    Button up,down,change,undo;
+    DrawableViewConfig config;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawableView = (DrawableView)findViewById(R.id.paintView);
+
+        up=(Button)findViewById(R.id.buttonUp);
+        down=(Button)findViewById(R.id.buttonDown);
+        change=(Button)findViewById(R.id.buttonColor);
+        undo=(Button)findViewById(R.id.buttonUndo);
+
+        config = new DrawableViewConfig();
+        config.setStrokeColor(getResources().getColor(android.R.color.black));
+        config.setShowCanvasBounds(true);
+        config.setMinZoom(1.0f);
+        config.setMaxZoom(3.0f);
+        config.setCanvasHeight(1920);
+        config.setCanvasWidth(1080);
+        drawableView.setConfig(config);
+
+        up.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                config.setStrokeWidth(config.getCanvasWidth() + 10);
+        }});
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                config.setStrokeWidth(config.getCanvasWidth() - 10);
+            }
+        });
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                config.setStrokeColor(Color.argb(255,random.nextInt(256),random.nextInt(256),random.nextInt(256)));
+            }
+        });
+        undo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawableView.undo();
+
+            }
+        });
+
+    }
+}
